7ed0f54d3d4ee5a343585d6100790cfbcc3feff7
Raminta Razauskaite <r.razauskaite@gmail.com>
2021-11-25 14:46:59 +0000

    Created form to add new event

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,7 +1,25 @@
 from flask import render_template, request
 from app import app
-from models.events import events
+from models.events import events, add_new_event
+from models.event import Event
 
 @app.route('/events')
 def index():
     return render_template('index.html', title="Events", events=events)
+
+
+@app.route('/events', methods=["POST"])
+def add_event():
+    # return render_template()
+
+    # Create a new task
+    event_name = request.form["name_of_event"]
+    event_date = request.form["date"]
+    event_num_of_guests = int(request.form["number_of_guests"])
+    event_room_location = request.form["room_location"]
+    event_description = request.form["description"]
+    newEvent = Event(event_date, event_name, event_num_of_guests, event_room_location, event_description)
+    # Add task to the list
+    add_new_event(newEvent)
+    # Render the list
+    return index()
--- models/events.py
+++ models/events.py
@@ -4,3 +4,6 @@ event_1 = Event("30/11/2021", "CodeClan Party", 15, "Glasgow", "Party hard")
 event_2 = Event("25/12/2021", "Christmas Party", 20, "Edinburgh", "Unbox presents")
 
 events = [event_1, event_2]
+
+def add_new_event(event):
+    events.append(event)
